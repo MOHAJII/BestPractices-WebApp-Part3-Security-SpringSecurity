fc6e19fd06cfe0d60bc68cae3f8e5ee33b22ff2a
mohammedhaji <hmohammediaji@gmail.com>
2025-05-06 15:22:51 +0100

    add picture

---
+++ src/main/java/net/haji/springmvcthymeleafsecured/security/SecurityConfig_InMemoryAuth.java
@@ -0,0 +1,46 @@
+package net.haji.springmvcthymeleafsecured.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig_InMemoryAuth {
+
+    private final PasswordEncoder passwordEncoder;
+
+    // @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user3").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user4").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+        );
+    }
+    // @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/").permitAll())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/webjars/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .rememberMe(Customizer.withDefaults())
+                .exceptionHandling(exp ->
+                        exp.accessDeniedPage("/notAuthorized"));
+        return httpSecurity.build();
+    }
+}
--- src/main/java/net/haji/springmvcthymeleafsecured/security/SecurityConfig_JDBCAuth.java
+++ src/main/java/net/haji/springmvcthymeleafsecured/security/SecurityConfig_JDBCAuth.java
@@ -9,25 +9,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig_JDBCAuth {
 
     private final PasswordEncoder passwordEncoder;
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("user3").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("user4").password(passwordEncoder.encode("1234")).roles("USER").build(),
-                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
-        );
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
